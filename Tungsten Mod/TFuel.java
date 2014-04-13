a24878f3089cf3fbcc656b27eeebe7a5c99f3aae
populo <Staudigel_C15@moeller.org>
2014-04-13 12:45:58 -0400

    1.2
    more balance to ore mining and gave steroids to fuel

--- Tungsten Mod/TFuel.java
+++ Tungsten Mod/TFuel.java
@@ -3,14 +3,14 @@ package populo.mod.tungsten;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.IFuelHandler;
 
-public class MoltenBlock implements IFuelHandler {
+public class TFuel implements IFuelHandler {
 
 	@Override
 	public int getBurnTime(ItemStack fuel) {
 		if(fuel.itemID == Tungsten.moltenBlock.blockID)
-			return 30000;
+			return 100000;
 		else if (fuel.itemID == Tungsten.molten.itemID)
-			return 10000;
+			return 75000;
 		else
 			return 0;
 	}
--- Tungsten Mod/block/Ore.java
+++ Tungsten Mod/block/Ore.java
@@ -16,18 +16,7 @@ public class Ore extends Block {
 		return Tungsten.ore.blockID;
 	}
 	public int quantityDropped(Random random) {
-		int y;
-		double x = Math.random();
-		if (x <= 0.33) {
-			y = 1;
-		} else if (x <= 0.66 && x > 0.33) {
-			y = 2;
-		} else if (x < 0.66) {
-			y = 3;
-		} else {
-			y = 4;
-		}
-		return y;
+		return 1+random.nextInt(1);
 	}
 	
 	public void registerIcons(IconRegister reg) {
